c686c5045420cb444466554541b6f650feeacf53
zyongan <yonganzhong@outlook.com>
2019-11-21 18:27:57 +0800

    improve resnet50 network

--- ResNet50_slim.py
+++ ResNet50_slim.py
@@ -40,16 +40,17 @@ class ResNet50():
         self.raw_input_label = tf.compat.v1.placeholder(tf.float32, shape=[None, self.num_classes], name="class_label")
         self.is_training = tf.compat.v1.placeholder_with_default(input=False, shape=(), name='is_training')
 
-        self.global_step = tf.Variable(0, trainable=False, name="global_step")
+        # self.global_step = tf.Variable(0, trainable=False, name="global_step")
+        self.global_step = tf.train.get_or_create_global_step()
         self.epoch_step = tf.Variable(0, trainable=False, name="epoch_step")
 
         # logits
-        self.logits = self.inference(self.raw_input_data, scope='InceptionV4')
+        self.logits = self.inference(self.raw_input_data, scope='resnet_v2_50')
         # # computer loss value
         self.loss = self.losses(labels=self.raw_input_label, logits=self.logits, scope='Loss')
         # train operation
         self.train = self.training(self.learning_rate, self.global_step, loss=self.loss)
-        self.train_accuracy = self.evaluate_batch(self.logits, self.raw_input_label) / batch_size
+        self.accuracy = self.evaluate_batch(self.logits, self.raw_input_label) / batch_size
 
     def inference(self, inputs, scope='resnet_v2_50'):
         """
@@ -89,15 +90,17 @@ class ResNet50():
                     normalizer_fn=slim.layers.batch_norm,
                     normalizer_params=batch_norm_params):
                 with slim.arg_scope([slim.batch_norm], **batch_norm_params):
-                    # with slim.arg_scope([slim.max_pool2d], padding='VALID') as arg_sc:
                     with slim.arg_scope([slim.max_pool2d], padding='SAME') as arg_sc:
                         with slim.arg_scope([slim.batch_norm], is_training=is_training):
                             net = self.resnet50_base(inputs=inputs, scope=sc)
                             # batch normalize
                             net = slim.batch_norm(net, activation_fn=tf.nn.relu, scope='postnorm')
-
-                            logits = slim.conv2d(inputs=net, num_outputs=num_classes, activation_fn=None,
-                                                 normalizer_fn=None, scope='logits')
+                            # average pool
+                            net = tf.reduce_mean(input_tensor=net, axis=[1, 2], name='pool5', keep_dims=True)
+                            # logits
+                            logits = slim.conv2d(inputs=net, num_outputs=num_classes, kernel_size=[1, 1],
+                                                 activation_fn=None, normalizer_fn=None, scope='logits')
+                            # squeeze
                             logits = tf.squeeze(input=logits, axis=[1, 2], name='SpatialSqueeze')
                             # softmax
                             prop = slim.softmax(logits, scope='predict')
@@ -112,29 +115,35 @@ class ResNet50():
         """
         with tf.compat.v1.variable_scope(scope, default_name='resnet_v2_50', values=[inputs]):
             with slim.arg_scope([slim.conv2d], activation_fn=None, normalizer_fn=None):
+                # 224 x 224 x 3
                 net = self.conv2d_same(inputs=inputs, kernel_size=[7, 7], num_outputs=64, stride=2,
                                        scope='conv1')
+            # 112 x 112 x 3
             net = slim.max_pool2d(inputs=net, kernel_size=[3, 3], stride=2, scope='pool1')
             # block 1
+            # 56 x 56 x 3
             with tf.variable_scope('block1', 'block') as sc:
                 net = self.conv_block(inputs=net, filters=[64, 64, 256], stride=2, scope='unit_1')
-                net = self.identity_block(inputs=net, filters=[64, 64, 256], stride=2, scope='unit_2')
-                net = self.identity_block(inputs=net, filters=[64, 64, 256], stride=2, scope='unit_3')
+                net = self.identity_block(inputs=net, filters=[64, 64, 256], stride=1, scope='unit_2')
+                net = self.identity_block(inputs=net, filters=[64, 64, 256], stride=1, scope='unit_3')
             # block 2
+            # 28 x 28 x 3
             with tf.variable_scope('block2', 'block') as sc:
                 net = self.conv_block(inputs=net, filters=[128, 128, 512], stride=2, scope='unit_1')
-                net = self.identity_block(inputs=net, filters=[128, 128, 512], stride=2, scope='unit_2')
-                net = self.identity_block(inputs=net, filters=[128, 128, 512], stride=2, scope='unit_3')
-                net = self.identity_block(inputs=net, filters=[128, 128, 512], stride=2, scope='unit_4')
+                net = self.identity_block(inputs=net, filters=[128, 128, 512], stride=1, scope='unit_2')
+                net = self.identity_block(inputs=net, filters=[128, 128, 512], stride=1, scope='unit_3')
+                net = self.identity_block(inputs=net, filters=[128, 128, 512], stride=1, scope='unit_4')
             # block 3
+            # 14 x 14 x 3
             with tf.variable_scope('block3', 'block') as sc:
                 net = self.conv_block(inputs=net, filters=[256, 256, 1024], stride=2, scope='unit_1')
-                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=2, scope='unit_2')
-                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=2, scope='unit_3')
-                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=2, scope='unit_4')
-                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=2, scope='unit_5')
-                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=2, scope='unit_6')
+                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=1, scope='unit_2')
+                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=1, scope='unit_3')
+                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=1, scope='unit_4')
+                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=1, scope='unit_5')
+                net = self.identity_block(inputs=net, filters=[256, 256, 1024], stride=1, scope='unit_6')
             # block 4
+            # 7 x 7 x 3
             with tf.variable_scope('block4', 'block') as sc:
                 net = self.conv_block(inputs=net, filters=[512, 512, 2048], stride=1, scope='unit_1')
                 net = self.identity_block(inputs=net, filters=[512, 512, 2048], stride=1, scope='unit_2')
@@ -153,26 +162,24 @@ class ResNet50():
         """
 
         with tf.variable_scope(scope, default_name='bottleneck_v2', values=[inputs]):
-            with slim.arg_scope([slim.conv2d], strides=1, padding='SAME'):
-                # get filters num
-                filter0, filter1, filter2 = filters
-                preact = slim.batch_norm(inputs=inputs, activation_fn=tf.nn.relu, scope='prect')
-                # shortcut net
-                shortcut = slim.conv2d(inputs=preact, num_outputs=filter0, kernel_size=(1, 1), stride=stride,
-                                       padding='SAME', scope='shortcut')
+            # get filters num
+            filter0, filter1, filter2 = filters
+            preact = slim.batch_norm(inputs=inputs, activation_fn=tf.nn.relu, scope='preact')
+            # shortcut net
+            shortcut = slim.conv2d(inputs=preact, num_outputs=filter2, kernel_size=[1, 1], stride=stride,
+                                   normalizer_fn=None, activation_fn=None, padding='SAME',  scope='shortcut')
+            # stack net
+            net = slim.conv2d(inputs=preact, num_outputs=filter0, kernel_size=[1, 1], stride=1, padding='SAME',
+                              scope='conv1')
+            net = self.conv2d_same(inputs=net, num_outputs=filter1, kernel_size=[3, 3], stride=stride, rate=rate,
+                                   scope='conv2')
+            net = slim.conv2d(inputs=net, num_outputs=filter2, kernel_size=[1, 1], stride=1, normalizer_fn=None,
+                              activation_fn=None, padding='SAME', scope='conv3')
 
-                # stack net
-                net = slim.conv2d(inputs=preact, num_outputs=filter0, kernel_size=(1, 1), stride=stride, padding='SAME',
-                                  scope='conv1')
-                net = self.conv2d_same(inputs=net, num_outputs=filter1, kernel_size=(3, 3), stride=stride, rate=rate,
-                                       scope='conv2')
-                net = slim.conv2d(inputs=net, num_outputs=filter2, kernel_size=(1, 1), strides=1, normalizer_fn=None,
-                                  activation_fn=None, padding='SAME', scope='conv3')
+            output = shortcut + net
+            return output
 
-                output = shortcut + net
-                return output
-
-    def identity_block(self, inputs, filters, stride=2, rate=1, scope=None):
+    def identity_block(self, inputs, filters, stride=1, rate=1, scope=None):
         """
         conv_block is the block that has no conv layer at shortcut
         :param inputs:
@@ -183,19 +190,19 @@ class ResNet50():
         :return:
         """
         with tf.variable_scope(scope, default_name='bottleneck_v2', values=[inputs]):
-            with slim.arg_scope([slim.conv2d], strides=1, padding='SAME'):
+            with slim.arg_scope([slim.conv2d], stride=1, padding='SAME'):
                 # get filters num
                 filter0, filter1, filter2 = filters
-                preact = slim.batch_norm(inputs=inputs, activation_fn=tf.nn.relu, scope='prect')
+                preact = slim.batch_norm(inputs=inputs, activation_fn=tf.nn.relu, scope='preact')
                 # shortcut net
                 shortcut = self.subsample(inputs=inputs, factor=stride, scope='shortcut')
 
                 # stack net
-                net = slim.conv2d(inputs=preact, num_outputs=filter0, kernel_size=(1, 1), stride=stride, padding='SAME',
+                net = slim.conv2d(inputs=preact, num_outputs=filter0, kernel_size=[1, 1], stride=1, padding='SAME',
                                   scope='conv1')
-                net = self.conv2d_same(inputs=net, num_outputs=filter1, kernel_size=(3, 3), stride=stride, rate=rate,
+                net = self.conv2d_same(inputs=net, num_outputs=filter1, kernel_size=[3, 3], stride=stride, rate=rate,
                                        scope='conv2')
-                net = slim.conv2d(inputs=net, num_outputs=filter2, kernel_size=(1, 1), strides=1, normalizer_fn=None,
+                net = slim.conv2d(inputs=net, num_outputs=filter2, kernel_size=[1, 1], stride=1, normalizer_fn=None,
                                   activation_fn=None, padding='SAME', scope='conv3')
                 output = shortcut + net
                 return output
@@ -216,11 +223,13 @@ class ResNet50():
             return slim.conv2d(inputs=inputs, num_outputs=num_outputs, kernel_size=kernel_size,
                                stride=stride, padding='SAME', scope=scope)
         else:
-            kernel_size_effect = kernel_size[0] * (kernel_size[0] - 1) * (rate - 1)
+            kernel_size_effect = kernel_size[0] + (kernel_size[0] - 1) * (rate - 1)
             pad_total = kernel_size_effect - 1
             pad_begin = pad_total // 2
             pad_end = pad_total - pad_begin
             inputs = tf.pad(inputs, paddings=[[0, 0], [pad_begin, pad_end], [pad_begin, pad_end], [0, 0]])
+
+            # int((n - k) / 2) + 1
             return slim.conv2d(inputs=inputs, num_outputs=num_outputs, kernel_size=kernel_size,
                                stride=stride, padding='VALID', scope=scope)
 
@@ -233,9 +242,9 @@ class ResNet50():
         :return:
         """
         if factor == 1:
-            return input
+            return inputs
         else:
-            return slim.max_pool2d(inputs=inputs, kernel_size=(1, 1), stride=factor, scope=scope )
+            return slim.max_pool2d(inputs=inputs, kernel_size=[1, 1], stride=factor, scope=scope)
 
 
     def training(self, learnRate, globalStep, loss):
