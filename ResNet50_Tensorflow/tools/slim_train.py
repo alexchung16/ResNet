24acba8e7363c5bd0547d97ff77f9a72d78a3ac0
alexchung <yonganzhong@outlook.com>
2020-06-17 15:01:06 +0800

    update train module

--- ResNet50_Tensorflow/tools/slim_train.py
+++ ResNet50_Tensorflow/tools/slim_train.py
@@ -6,23 +6,24 @@
 # @ Time 21/11/2019 PM 15:44
 
 import os
+from datetime import datetime
 import numpy as np
 import tensorflow as tf
-# from Inception.Inception_v3 import InceptionV3
-from ResNet50_Tensorflow.ResNet50_slim import ResNet50
-from DataProcess.read_TFRecord import reader_tfrecord, get_num_samples
-from tensorflow.python.framework import graph_util
-#
-original_dataset_dir = '/home/alex/Documents/datasets/flower_photos_separate'
-tfrecord_dir = os.path.join(original_dataset_dir, 'tfrecord')
 
-train_path = os.path.join(original_dataset_dir, 'train')
-test_path = os.path.join(original_dataset_dir, 'test')
-record_file = os.path.join(tfrecord_dir, 'image.tfrecords')
-model_path = os.path.join(os.getcwd(), 'model')
-model_name = os.path.join(model_path, 'resnet_50.pb')
-pretrain_model_dir = '/home/alex/Documents/pretraing_model/resnet/resnet_50/resnet_v2_50.ckpt'
-logs_dir = os.path.join(os.getcwd(), 'logs')
+from ResNet50_Tensorflow.nets.ResNet50_slim import ResNet50
+from DataProcess.read_TFRecord import dataset_tfrecord, get_num_samples
+
+# dataset path
+dataset_dir = '/home/alex/Documents/dataset/flower_tfrecord'
+train_data_path = os.path.join(dataset_dir, 'train')
+test_data_path = os.path.join(dataset_dir, 'val')
+
+# pretrain model
+pretrain_model_path = '/home/alex/Documents/pretrain_model/resnet/resnet_50/resnet_v2_50.ckpt'
+
+# output path
+logs_dir = os.path.join('../', 'outputs', 'logs')
+model_dir = save_dir = os.path.join('../', 'outputs', 'model')
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -30,25 +31,26 @@ flags.DEFINE_integer('height', 224, 'Number of height size.')
 flags.DEFINE_integer('width', 224, 'Number of width size.')
 flags.DEFINE_integer('depth', 3, 'Number of depth size.')
 flags.DEFINE_integer('num_classes', 5, 'Number of image class.')
+flags.DEFINE_integer('batch_size', 32, 'Batch size Must divide evenly into the dataset sizes.')
 flags.DEFINE_integer('epoch', 30, 'Number of epoch size.')
-flags.DEFINE_integer('step_per_epoch', 100, 'Number of step size of per epoch.')
-flags.DEFINE_float('learning_rate', 1e-3, 'Initial learning rate.')
-flags.DEFINE_float('decay_rate', 0.99, 'Number of learning decay rate.')
-# flags.DEFINE_bool('global_pool', False, 'if True, use global pool.')
-# flags.DEFINE_bool('spacial_squeeze', True, 'if True, execute squeeze.')
-flags.DEFINE_integer('num_epoch_per_decay', 2, 'Number epoch after each leaning rate decapy.')
-flags.DEFINE_float('keep_prob', 0.8, 'Number of probability that each element is kept.')
+flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
+flags.DEFINE_float('momentum', 0.9, 'momentum rate.')
+flags.DEFINE_float('decay_rate', 0.1, 'Number of learning decay rate.')
+flags.DEFINE_integer('num_epoch_per_decay', 20, 'Number epoch after each leaning rate decay.')
+
 flags.DEFINE_float('weight_decay', 0.0001, 'Number of weight decay with regular')
 flags.DEFINE_float('batch_norm_decay', 0.997, 'Number of batch norm decay.')
 flags.DEFINE_float('batch_norm_epsilon', 1e-5, 'Number of batch norm epsilon.')
 flags.DEFINE_bool('batch_norm_scale', True, 'if True, use gamma for update.')
 flags.DEFINE_bool('batch_norm_fused', True, 'if True, use a faster, fused implementation if possible.')
-
-flags.DEFINE_string('train_dir', record_file, 'Directory to put the training data.')
 flags.DEFINE_bool('is_pretrain', True, 'if True, use pretrain model.')
-flags.DEFINE_string('pretrain_model_dir', pretrain_model_dir, 'pretrain model dir.')
+
+flags.DEFINE_string('train_data_dir', train_data_path, 'Directory to put the training data.')
+flags.DEFINE_string('test_data_dir', test_data_path, 'Directory to put the training data.')
 flags.DEFINE_string('logs_dir', logs_dir, 'direct of summary logs.')
-flags.DEFINE_string('model_name', model_name, 'model_name.')
+flags.DEFINE_string('model_dir', model_dir, 'direct of summary model to save.')
+flags.DEFINE_integer('save_step_period', 2000, 'save model step period')
+flags.DEFINE_string('pretrain_model_path', pretrain_model_path, 'pretrain model path.')
 
 def predict(model_name, image_data, input_op_name, predict_op_name):
     """
@@ -104,109 +106,164 @@ def networkStructureTest(batch_size):
 #
 if __name__ == "__main__":
 
-    num_samples = get_num_samples(record_file=FLAGS.train_dir)
-    batch_size = num_samples // FLAGS.step_per_epoch
+    num_train_samples = get_num_samples(record_dir=FLAGS.train_data_dir)
+    num_val_samples = get_num_samples(record_dir=FLAGS.test_data_dir)
+
+    # get total step of the number train epoch
+    step_per_epoch = num_train_samples // FLAGS.batch_size  # get num step of per epoch
+    max_step = FLAGS.epoch * step_per_epoch  # get total step of several epoch
 
     resnet_50 = ResNet50(input_shape=[FLAGS.height, FLAGS.width, FLAGS.depth],
                                num_classes=FLAGS.num_classes,
-                               batch_size=batch_size,
-                               decay_rate=FLAGS.decay_rate,
+                               batch_size=FLAGS.batch_size,
                                learning_rate=FLAGS.learning_rate,
-                               num_samples_per_epoch=num_samples,
+                               momentum=FLAGS.momentum,
+                               decay_rate=FLAGS.decay_rate,
+                               num_samples_per_epoch=num_train_samples,
                                num_epoch_per_decay=FLAGS.num_epoch_per_decay,
-                               keep_prob=FLAGS.keep_prob,
                                weight_decay=FLAGS.weight_decay,
                                batch_norm_decay=FLAGS.batch_norm_decay,
                                batch_norm_epsilon=FLAGS.batch_norm_epsilon,
-                               batch_norm_fused=FLAGS.batch_norm_fused,
-                               is_pretrain=FLAGS.is_pretrain
-                               )
+                               batch_norm_scale=FLAGS.batch_norm_scale,
+                               batch_norm_fused=FLAGS.batch_norm_fused)
 
     # add scalar value to summary protocol buffer
     tf.summary.scalar('loss', resnet_50.loss)
     tf.summary.scalar('accuracy', resnet_50.accuracy)
 
-    # networkStructureTest(batch_size=batch_size)
-    images, labels, filenames = reader_tfrecord(record_file=FLAGS.train_dir,
-                                                batch_size=batch_size,
-                                                input_shape=[FLAGS.height, FLAGS.width, FLAGS.depth],
-                                                class_depth=FLAGS.num_classes,
-                                                epoch=FLAGS.epoch,
-                                                shuffle=False)
+    train_images_batch, train_labels_batch, train_filenames = dataset_tfrecord(record_files=FLAGS.train_data_dir,
+                                                                               batch_size=FLAGS.batch_size,
+                                                                               target_shape=[FLAGS.height, FLAGS.width,
+                                                                                             FLAGS.depth],
+                                                                               class_depth=FLAGS.num_classes,
+                                                                               epoch=FLAGS.epoch,
+                                                                               shuffle=True,
+                                                                               is_training=True)
+    val_images_batch, val_labels_batch, val_filenames = dataset_tfrecord(record_files=FLAGS.test_data_dir,
+                                                                         batch_size=FLAGS.batch_size,
+                                                                         target_shape=[FLAGS.height, FLAGS.width,
+                                                                                       FLAGS.depth],
+                                                                         class_depth=FLAGS.num_classes,
+                                                                         epoch=FLAGS.epoch,
+                                                                         shuffle=True,
+                                                                         is_training=False)
+    saver = tf.train.Saver()
     init_op = tf.group(
         tf.global_variables_initializer(),
         tf.local_variables_initializer()
     )
+
+    # os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+    config = tf.ConfigProto()
+    # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # maximun alloc gpu50% of MEM
+    config.gpu_options.allow_growth = True
     # train and save model
-
-    with tf.Session() as sess:
+    with tf.Session(config=config) as sess:
         sess.run(init_op)
-
         # get computer graph
         graph = tf.get_default_graph()
+        logit_op = resnet_50.logits.op.name
         write = tf.summary.FileWriter(logdir=FLAGS.logs_dir, graph=graph)
         # get model variable of network
         model_variable = tf.model_variables()
         for var in model_variable:
-            print(var.name)
-
+            print(var.op.name)
+            print(var.shape)
         # get and add histogram to summary protocol buffer
         logit_weight = graph.get_tensor_by_name(name='resnet_v2_50/logits/weights:0')
         tf.summary.histogram(name='logits/weight', values=logit_weight)
-        logit_biase = graph.get_tensor_by_name(name='resnet_v2_50/logits/biases:0')
-        tf.summary.histogram(name='logits/biases', values=logit_biase)
+        logit_biases = graph.get_tensor_by_name(name='resnet_v2_50/logits/biases:0')
+        tf.summary.histogram(name='logits/biases', values=logit_biases)
         # merges all summaries collected in the default graph
         summary_op = tf.summary.merge_all()
         # load pretrain model
         if FLAGS.is_pretrain:
             # remove variable of fc8 layer from pretrain model
-            custom_scope = ['resnet_v2_50/logits']
-            for scope in custom_scope:
-                variables = tf.model_variables(scope=scope)
-                [model_variable.remove(var) for var in variables]
-            saver = tf.train.Saver(var_list=model_variable)
-            saver.restore(sess, save_path=FLAGS.pretrain_model_dir)
+            resnet_50.load_weights(sess, model_path=FLAGS.pretrain_model_path)
 
+        # print(sess.run('vgg_16/conv1/conv1_1/biases:0'))
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
         try:
             if not coord.should_stop():
-                for epoch in range(FLAGS.epoch):
-                    print('Epoch: {0}/{1}'.format(epoch, FLAGS.epoch))
-                    for step in range(FLAGS.step_per_epoch):
 
-                        image, label, filename = sess.run([images, labels, filenames])
+                # ++++++++++++++++++++++++++++++++++start training+++++++++++++++++++++++++++++++++++++++++++++++++
+                # used to record the number step of per epoch
+                step_epoch = 0
+                for step in range(max_step):
+                    # --------------------------------print number of epoch--------------------------------------
+                    if (step) % step_per_epoch == 0:
+                        tmp_epoch = (step + 1) // step_per_epoch
+                        print('Epoch: {0}/{1}'.format(tmp_epoch + 1, FLAGS.epoch))
 
-                        feed_dict = resnet_50.fill_feed_dict(image_feed=image,
-                                                                label_feed=label,
-                                                                is_training=True)
+                    # +++++++++++++++++++++++++++++++train part++++++++++++++++++++++++++++++++++++++++++++++++
+                    train_image, train_label = sess.run([train_images_batch, train_labels_batch])
 
-                        _, loss_value, train_accuracy, summary = sess.run(
-                            fetches=[resnet_50.train, resnet_50.loss, resnet_50.accuracy, summary_op],
-                            feed_dict=feed_dict)
+                    feed_dict = resnet_50.fill_feed_dict(image_feed=train_image, label_feed=train_label,
+                                                         is_training=True)
 
-                        print('  Step {0}/{1}: loss value {2}  train accuracy {3}'
-                              .format(step, FLAGS.step_per_epoch, loss_value, train_accuracy))
+                    _, train_loss, train_accuracy, summary = sess.run(fetches=[resnet_50.train, resnet_50.loss,
+                                                                               resnet_50.accuracy, summary_op],
+                                                                      feed_dict=feed_dict)
+                    write.add_summary(summary=summary, global_step=step)
 
-                        write.add_summary(summary=summary, global_step=epoch*FLAGS.step_per_epoch+step)
+                    step_epoch += 1
+                    # print training info
+                    print(
+                        '\tstep {0}:loss value {1}  train accuracy {2}'.format(step_epoch, train_loss, train_accuracy))
+
+                    # -------------------------save_model every  save_step_period--------------------------------
+                    if (step + 1) % FLAGS.save_step_period == 0:
+                        saver.save(sess, save_path=os.path.join(FLAGS.model_dir, 'model.ckpt'),
+                                   global_step=resnet_50.global_step)
+                    # ++++++++++++++++++++++++++++++++validation part++++++++++++++++++++++++++++++++++++++++++++
+                    # execute validation when complete every epoch
+                    # validation use with all validation dataset
+                    if (step + 1) % step_per_epoch == 0:
+                        val_losses = []
+                        val_accuracies = []
+                        val_max_steps = int(num_val_samples / FLAGS.batch_size)
+                        for _ in range(val_max_steps):
+                            val_images, val_labels = sess.run([val_images_batch, val_labels_batch])
+
+                            feed_dict = resnet_50.fill_feed_dict(image_feed=val_images, label_feed=val_labels,
+                                                                 is_training=False)
+
+                            val_loss, val_acc = sess.run([resnet_50.loss, resnet_50.accuracy],
+                                                         feed_dict=feed_dict)
+
+                            val_losses.append(val_loss)
+                            val_accuracies.append(val_acc)
+                        mean_loss = np.array(val_losses, dtype=np.float32).mean()
+                        mean_acc = np.array(val_accuracies, dtype=np.float32).mean()
+
+                        print("\t{0}: epoch: {1}  val Loss: {2}, val accuracy:  {3}".format(datetime.now(),
+                                                                                            (step + 1) // step_per_epoch,
+                                                                                            mean_loss, mean_acc))
+                        step_epoch = 0  # update step_epoch
+
+                saver.save(sess, save_path=os.path.join(FLAGS.model_dir, 'model.ckpt'),
+                           global_step=resnet_50.global_step)
                 write.close()
 
-                # save model
+
+                # ++++++++++++++++++++++++++++save model to pb+++++++++++++++++++++++++++++++++++++++
                 # get op name for save model
-                input_op = resnet_50.raw_input_data.name
-                logit_op = resnet_50.logits.name
+                # input_op = vgg.raw_input_data.name
+                # logit_op = vgg.logits.name
+                input_op = resnet_50.raw_input_data.op.name
+                logit_op = resnet_50.logits.op.name
                 # convert variable to constant
-                def_graph = tf.get_default_graph().as_graph_def()
-                constant_graph = tf.graph_util.convert_variables_to_constants(sess, def_graph,
-                                                                              [input_op.split(':')[0],
-                                                                               logit_op.split(':')[0]])
+                input_graph_def = tf.get_default_graph().as_graph_def()
+                constant_graph = tf.graph_util.convert_variables_to_constants(sess, input_graph_def,
+                                                                              [input_op, logit_op])
                 # save to serialize file
-                with tf.gfile.FastGFile(name=FLAGS.model_name, mode='wb') as f:
+                with tf.gfile.FastGFile(name=os.path.join(FLAGS.model_dir, 'model.pb'), mode='wb') as f:
                     f.write(constant_graph.SerializeToString())
 
         except Exception as e:
             print(e)
         coord.request_stop()
-    coord.join(threads)
+        coord.join(threads)
     sess.close()
     print('model training has complete')
